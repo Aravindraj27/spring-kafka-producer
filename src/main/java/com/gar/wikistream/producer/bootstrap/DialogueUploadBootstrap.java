6356b988340dc480241fa546f75298aea9d62b41
Aravindraj Ganesan <aravindrajg@outlook.com>
2024-02-05 07:53:58 +0530

    Adding SQL Properties and Data Bootstrapping

---
+++ src/main/java/com/gar/wikistream/producer/bootstrap/DialogueUploadBootstrap.java
@@ -0,0 +1,10 @@
+package com.gar.wikistream.producer.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DialogueUploadBootstrap implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
