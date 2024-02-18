97118f3ad3cbe7d29408282acb1f59d02fa9d2a3
Aravindraj Ganesan <aravindrajg@outlook.com>
2024-02-18 21:05:03 +0530

    Adding mongo config and bootstrapping data

--- src/main/java/com/gar/wikistream/producer/model/Dialogues.java
+++ src/main/java/com/gar/wikistream/producer/model/Dialogues.java
@@ -1,22 +1,20 @@
 package com.gar.wikistream.producer.model;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document
+@Document(collection = "Dialogues")
 @Getter
 @Setter
 @AllArgsConstructor
 @ToString
+@NoArgsConstructor
 public class Dialogues {
     @Id
     private String id;
-    private int actNumber;
+    private String playId;
     private String speaker;
     private String tone;
     private String dialogue;
